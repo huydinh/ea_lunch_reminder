347e7e9bbcdf280828b38286b9fabfb1d327825c
HieuN+HuyD <hieu.nguyen+huy.dinh@eastagile.com>
2013-06-06 18:42:10 +0700

    add new tab listener

--- event.js
+++ event.js
@@ -1,6 +1,25 @@
+alert(1);
 function checkLunchTime() {
-	alert(1);
-	setTimeout(checkLunchTime, 5000);
+	var date = new Date;
+	hours = date.getHours();
+	minutes = date.getMinutes();
+  if(hours == 13 && (minutes > 0 && minutes < 45)) {
+		alert("Have your ordered lunch!");
+
+		sleepLunchCheck();
+	}else {
+		setTimeout(checkLunchTime, 10 * 60 * 1000);
+	}
+}
+
+function sleepLunchCheck() {
+	setTimeout(function() {
+		checkLunchTime();
+	}, 20 * 60 * 60 * 1000);
 }
 
 checkLunchTime();
+
+chrome.tabs.onCreated.addListener(function(tab) {
+	alert('tab');
+});
